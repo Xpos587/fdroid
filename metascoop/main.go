b10b7a3e908a0648921e71ec06ea2d287fb2ec1d
Michael <x30827pos@gmail.com>
2025-06-18 14:39:30 +0300

    chore: attempt #32

--- metascoop/main.go
+++ metascoop/main.go
@@ -108,8 +108,11 @@ func main() {
 				defer fmt.Println("::endgroup::")
 
 				if release.GetPrerelease() {
-					log.Printf("Skipping prerelease %q", release.GetTagName())
-					return
+					if !strings.Contains(app.GitURL, "hiddify/hiddify-app") {
+						log.Printf("Skipping prerelease %q", release.GetTagName())
+						return
+					}
+					log.Printf("Processing prerelease %q for Hiddify", release.GetTagName())
 				}
 				if release.GetDraft() {
 					log.Printf("Skipping draft %q", release.GetTagName())
